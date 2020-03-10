b96c499e71778b5142ba1bfd5daa5c6b5b28a514
Edison Vasquez <edisonvasquezd@gmail.com>
2020-03-10 17:05:53 -0300

    hice un boton infinito

--- primera_app/lib/pages/home_page.dart
+++ primera_app/lib/pages/home_page.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final estiloTexto = new TextStyle(fontSize: 40);
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titulo'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Hola mundo de flutter',
+              style: estiloTexto,
+            ),
+            Text(
+              '0',
+              style: estiloTexto,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.all_inclusive),
+        onPressed: () {
+          print('Hola mundo');
+        },
+      ),
+    );
   }
 }
--- primera_app/lib/src/app.dart
+++ primera_app/lib/src/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:primeraapp/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
-      home: Center(child: Text('Hola mundo al fin estoy en flutter!!')),
+      home: Center(
+        child: HomePage(),
+      ),
     );
   }
 }
